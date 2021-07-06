702b1d334ba2f0e209b136e5dad22e7dc7b6ed70
jose-tello <47975174+jose-tello@users.noreply.github.com>
2021-07-06 12:41:46 +0200

    value prefabs added

---
+++ Assets/Scripts/ValueToSelect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ValueToSelect : MonoBehaviour
+{
+    private Text txt = null;
+    public GameObject textChildGO = null;
+
+    void Start()
+    {
+        if (textChildGO != null)
+        {
+            txt = textChildGO.GetComponent<Text>();
+
+            if (txt == null)
+            {
+                Debug.Log("Need to add text component!");
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("Need to add child with text component!");
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ValueSelected()
+    {
+        Debug.Log("Selected a value!");
+    }
+
+}
